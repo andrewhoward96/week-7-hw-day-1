93cb963472d32cb41144b748305b5a74eb7b3474
andrewhoward96 <andrewhoward1996@hotmail.co.uk>
2022-06-13 22:52:08 +0100

    hw

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState }  from 'react';
 
 function App() {
+
+  const [category, setcategory] =useState([
+    {name: "Buy shopping", priority: "high"},
+    {name: "Clean bathroom", priority: "low"},
+    {name: "Car's MOT", priority: "high"},
+  ]);
+
+  const[newCategory, setNewCategory] = useState("");
+  const[priority, setNewPriority] = useState("");
+
+  const categoryNodes = category.map((category,index)=> {
+    return(
+      <li key={index} className={category.priority === 'high' ? "high-priority": "low-priority"}><span>{category.name}</span>
+      </li>
+    )
+  })
+
+
+  const handecategoryInput = (event) => {
+    setNewCategory(event.target.value)
+  }
+
+  const handleRadioSelect = (event) => {
+    setNewPriority(event.target.value)
+  }
+
+  const savenewCategory = (event) => {
+    event.preventDefault();
+    const copycategory = [...category]
+    copycategory.push({name: newCategory, priority})
+    setcategory(copycategory)
+    setNewCategory("")
+  }
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>ToDo's</h1>
+
+      <hr></hr>
+
+      <ul>
+        {categoryNodes}
+      </ul>
+
+      <form onSubmit={savenewCategory}>
+      <label htmlFor="new-category"></label>
+      <input id="new-category" type="text" value={newCategory} onChange={handecategoryInput} />
+      <input type="submit" value="New Category"/>
+      <div className='radio-buttons' onChange={handleRadioSelect}>
+        <label>High</label>
+        <input id='high-radio' type="radio" value="high" name='priority'></input>
+        <label>Low</label>
+        <input id='low-radio' type="radio" value="low" name='priority'></input>
+      </div>
+
+      </form>
+
     </div>
   );
 }
